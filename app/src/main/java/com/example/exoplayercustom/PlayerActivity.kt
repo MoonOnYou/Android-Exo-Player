c35dfed904641b5413609becce5168a7d257ab60
onyou <onyou0031@naver.com>
2020-09-25 13:44:25 +0900

    일단 기본 플레이 성공

--- app/src/main/java/com/example/exoplayercustom/PlayerActivity.kt
+++ app/src/main/java/com/example/exoplayercustom/PlayerActivity.kt
@@ -7,37 +7,31 @@ import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-
+import kotlinx.android.synthetic.main.activity_player.*
 
 /**
+ *   참고 블로그
  *   https://selfish-developer.com/entry/Exoplayer2-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
  *   https://lcw126.tistory.com/117
  */
 class PlayerActivity : AppCompatActivity() {
     private val videoUri = Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4")
-    private var playerView :PlayerView? = null
     private var player : SimpleExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
 
-        playerView = findViewById(R.id.playerView)
-
     }
 
     override fun onStart() {
         super.onStart()
         // simple exo player 객체 생성
-        ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+        player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
 
         // 뷰에 플레이어 설정
         playerView?.player = player
-
-//        val factory = ExtractorMediaSource.Factory(DefaultDataSourceFactory(this, Util.getUserAgent(this, this.applicationInfo.packageName)))
-//        val mediaSource = factory.createMediaSource(videoUri)
 
         //비디오데이터 소스를 관리하는 DataSource 객체를 만들어주는 팩토리 객체 생성
         val factory = DefaultDataSourceFactory(this, this.applicationInfo.packageName)
