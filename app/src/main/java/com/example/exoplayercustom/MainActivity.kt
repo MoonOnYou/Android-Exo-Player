71f62461d70e0abc54bb3cd5d5559c0c6a42cd51
onyou <onyou0031@naver.com>
2020-09-25 09:28:28 +0900

    gitignore ㄲㅏ먹지 말자

---
+++ app/src/main/java/com/example/exoplayercustom/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.exoplayercustom
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textViewGoPlayer.setOnClickListener {
+            startActivity(Intent(this, PlayerActivity::class.java))
+        }
+    }
+}
